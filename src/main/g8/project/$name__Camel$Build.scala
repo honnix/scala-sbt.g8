07301a7c0034954a02bd084e92661a2d2bf48268
ehonlia <hongxin.liang@ericsson.com>
2013-01-09 10:13:10 +0100

    more configurations

--- src/main/g8/project/$name__Camel$Build.scala
+++ src/main/g8/project/$name__Camel$Build.scala
@@ -1,17 +1,79 @@
 import sbt._
-import sbt.Keys._
+import Keys._
+
+object Resolvers {
+  val typesafeReleases = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+  val mavenLocal = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
+
+  val myResolvers = Seq(typesafeReleases, mavenLocal)
+}
+
+object BuildSettings {
+
+  import Resolvers._
+
+  val buildOrganization = "$organization$"
+  val buildVersion = "$version$"
+  val buildScalaVersion = "$scala_version$"
+
+  val buildSettings = Defaults.defaultSettings ++ Seq(
+    organization := buildOrganization,
+    version := buildVersion,
+    scalaVersion := buildScalaVersion,
+    shellPrompt := ShellPrompt.buildShellPrompt,
+    // publish to maven repository
+    // publishTo := Some(Resolver.file("file", new File(Path.userHome.absolutePath + "/.m2/repository"))),
+    resolvers ++= myResolvers
+  )
+}
+
+object ShellPrompt {
+
+  object devnull extends ProcessLogger {
+    def info(s: => String) {}
+
+    def error(s: => String) {}
+
+    def buffer[T](f: => T): T = f
+  }
+
+  def currBranch = (
+    ("git status -sb" lines_! devnull headOption)
+      getOrElse "-" stripPrefix "## "
+    )
+
+  val buildShellPrompt = {
+    (state: State) => {
+      val currProject = Project.extract(state).currentProject.id
+      "%s:%s:%s> ".format(
+        currProject, currBranch, BuildSettings.buildVersion
+      )
+    }
+  }
+}
+
+object Dependencies {
+  // private val crossMappedDowngrade = CrossVersion.fullMapped {
+  //   case "2.10.0" => "2.9.2"
+  //   case x => x
+  // }
+
+  // val akka = "com.typesafe.akka" %% "akka-actor" % "2.1.0"
+}
 
 object $name;format="Camel"$Build extends Build {
+
+  import Dependencies._
+  import BuildSettings._
+
+  val commonDeps = Seq(
+    // akka
+  )
 
   lazy val $name;format="camel"$ = Project(
     id = "$name;format="norm"$",
     base = file("."),
-    settings = Project.defaultSettings ++ Seq(
-      name := "$name$",
-      organization := "$organization$",
-      version := "$version$",
-      scalaVersion := "$scala_version$"
-      // add other settings here
-    )
-  )
+    settings = buildSettings ++ Seq(
+      libraryDependencies ++= commonDeps
+    ))
 }
